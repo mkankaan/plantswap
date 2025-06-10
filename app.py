ff7e6bbbf7be5000f2800b98dde2cff41f28e8ac
Matleena Kankaanpää <104016919+mkankaan@users.noreply.github.com>
2025-06-11 00:09:40 +0300

    Add line breaks

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import render_template, request, redirect, session, abort, make_response, flash
 import db, users, config, listings, comments, images, cities
-import sqlite3, secrets
+import sqlite3, secrets, markupsafe
 from utils import form_validation, date_formatter
 
 app = Flask(__name__)
@@ -14,6 +14,12 @@ def require_login():
 def check_csrf():
     if request.form["csrf_token"] != session["csrf_token"]:
         abort(403)
+
+@app.template_filter()
+def show_lines(content):
+    content = str(markupsafe.escape(content))
+    content = content.replace("\n", "<br />")
+    return markupsafe.Markup(content)
 
 @app.route("/")
 @app.route("/recommendations")
