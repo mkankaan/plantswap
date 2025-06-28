27a88f74d249f8a7b72c50897dac9105b0bfe687
Matleena Kankaanpää <104016919+mkankaan@users.noreply.github.com>
2025-06-29 00:06:43 +0300

    Delete unused class

--- init.sql
+++ init.sql
@@ -1,7 +1,6 @@
 DELETE FROM classes;
 
 INSERT INTO classes (option_title, option_value) VALUES ('Tyyppi', 'Pistokas');
-INSERT INTO classes (option_title, option_value) VALUES ('Tyyppi', 'Kasvi');
 INSERT INTO classes (option_title, option_value) VALUES ('Valon tarve', 'Vähäinen');
 INSERT INTO classes (option_title, option_value) VALUES ('Valon tarve', 'Keskitaso');
 INSERT INTO classes (option_title, option_value) VALUES ('Valon tarve', 'Runsas');
