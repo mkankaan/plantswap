146415a08e5b1647ee68c70245858d588bbaf238
Matleena Kankaanpää <104016919+mkankaan@users.noreply.github.com>
2025-08-24 16:05:48 +0300

    Fix Pylint errors

--- utils/form_validation.py
+++ utils/form_validation.py
@@ -1,16 +1,24 @@
 import re
 
-registration_restrictions = { "min_username": 3, "max_username": 20, "min_password": 8, "max_password": 100, "max_city": 50 }
+registration_restrictions = { "min_username": 3,
+                             "max_username": 20,
+                             "min_password": 8,
+                             "max_password": 100,
+                             "max_city": 50 }
 
-new_listing_restrictions = { "max_name": 50, "max_info": 500 }
+new_listing_restrictions = { "max_name": 50,
+                            "max_info": 500 }
 
 listing_comment_restrictions = { "max_comment": 500 }
 
 form_hint_text = {
-    "username": f"Pituus {registration_restrictions['min_username']}-{registration_restrictions['max_username']} merkkiä",
-    "password": f"Pituus {registration_restrictions['min_password']}-{registration_restrictions['max_password']} merkkiä, vähintään yksi kirjain ja numero",
-    "city": f"Korkeintaan {registration_restrictions['max_city']} merkkiä",
-    "comment": f"Korkeintaan {listing_comment_restrictions['max_comment']} merkkiä"
+    "username": f"""Pituus {registration_restrictions['min_username']}-
+                {registration_restrictions['max_username']} merkkiä""",
+    "password": f"""Pituus {registration_restrictions['min_password']}-
+                {registration_restrictions['max_password']} merkkiä,
+                vähintään yksi kirjain ja numero""",
+    "city": f"""Korkeintaan {registration_restrictions['max_city']} merkkiä""",
+    "comment": f"""Korkeintaan {listing_comment_restrictions['max_comment']} merkkiä"""
 }
 
 def validate_username(username):
@@ -18,21 +26,21 @@ def validate_username(username):
     max_length = registration_restrictions["max_username"]
 
     if not min_length <= len(username) <= max_length:
-        message = "Käyttäjätunnuksen on oltava vähintään " + str(min_length) + " ja korkeintaan " + str(max_length) + " merkkiä pitkä"
+        message = f"""Käyttäjätunnuksen on oltava vähintään {str(min_length)}
+                    ja korkeintaan {str(max_length)} merkkiä pitkä"""
         return (False, message)
 
     if len(username) != len(username.replace(" ", "")):
         message = "Käyttäjätunnus ei saa sisältää välilyöntejä"
         return (False, message)
-    
+
     if not re.search(r"[A-Za-z]{1,}", username):
         message = "Käyttäjätunnuksen täytyy sisältää vähintään yksi kirjain"
         return (False, message)
-    
+
     if not (re.fullmatch(r"^[A-Za-z0-9]*$", username) or re.fullmatch(r"^[A-Za-z]*$", username)):
         message = "Käyttäjätunnuksessa saa olla vain kirjaimia ja numeroita"
         return (False, message)
-    
     return (True, "")
 
 def validate_password(password):
@@ -40,17 +48,17 @@ def validate_password(password):
     max_length = registration_restrictions["max_password"]
 
     if not min_length <= len(password) <= max_length:
-        message = "Salasanan on oltava vähintään " + str(min_length) + " ja korkeintaan " + str(max_length) + " merkkiä pitkä"
+        message = f"""Salasanan on oltava vähintään {str(min_length)}
+                    ja korkeintaan {str(max_length)} merkkiä pitkä"""
         return (False, message)
-    
+
     if len(password) != len(password.replace(" ", "")):
         message = "Salasana ei saa sisältää välilyöntejä"
         return (False, message)
-    
+
     if not (re.match(r"^(?=.*[a-zA-Z])", password) and re.match(r"^(?=.*[0-9])", password)):
         message = "Salasanan täytyy sisältää vähintään yksi kirjain ja yksi numero."
         return (False, message)
-    
     return (True, "")
 
 def validate_city(city):
@@ -58,9 +66,9 @@ def validate_city(city):
     city = city.strip()
 
     if not 1 <= len(city) <= max_length:
-        message = "Kaupungin nimen on oltava vähintään 1 ja korkeintaan " + str(max_length) + " merkkiä pitkä"
+        message = f"""Kaupungin nimen on oltava vähintään 1 ja korkeintaan
+                    {str(max_length)} merkkiä pitkä"""
         return (False, message)
-    
     return (True, "")
 
 def validate_comment(content):
@@ -73,5 +81,4 @@ def validate_comment(content):
     if len(content) > max_length:
         message = "Kommentin maksimipituus on " + str(max_length) + " merkkiä"
         return (False, message)
-    
     return (True, "")
